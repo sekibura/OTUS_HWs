f784c1c4e7b5e0bb908a2aa0965740fa8e396ff5
Petrov Dmitry <petrovdmtry@gmail.com>
2024-12-24 12:28:51 +0300

    refactored: BulletConfig

--- 4. GRASP/ShootEmUp (Dirty)/Assets/Scripts/Bullets/BulletConfig.cs
+++ 4. GRASP/ShootEmUp (Dirty)/Assets/Scripts/Bullets/BulletConfig.cs
@@ -8,16 +8,9 @@ namespace ShootEmUp
     )]
     public sealed class BulletConfig : ScriptableObject
     {
-        [SerializeField]
         public PhysicsLayer PhysicsLayer;
-
-        [SerializeField]
         public Color Color;
-
-        [SerializeField]
         public int Damage;
-
-        [SerializeField]
         public float Speed;
     }
 }
