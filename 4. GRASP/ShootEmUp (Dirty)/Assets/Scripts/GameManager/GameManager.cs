077fdd667a8b2440064bb51c3296ac6825fba121
Petrov Dmitry <petrovdmtry@gmail.com>
2024-12-24 14:13:44 +0300

    refactored: GameManager

--- 4. GRASP/ShootEmUp (Dirty)/Assets/Scripts/GameManager/GameManager.cs
+++ 4. GRASP/ShootEmUp (Dirty)/Assets/Scripts/GameManager/GameManager.cs
@@ -1,10 +1,25 @@
+using System;
+using ShootEmUp.Modules.Components;
 using UnityEngine;
 
 namespace ShootEmUp
 {
     public sealed class GameManager : MonoBehaviour
     {
-        public void FinishGame()
+        [SerializeField]
+        private HitPointsComponent _playerHitPointsController;
+
+        private void OnEnable()
+        {
+            _playerHitPointsController.OnDeath += FinishGame;
+        }
+
+        private void OnDisable()
+        {
+            _playerHitPointsController.OnDeath -= FinishGame;
+        }
+
+        public void FinishGame(GameObject go)
         {
             Debug.Log("Game over!");
             Time.timeScale = 0;
