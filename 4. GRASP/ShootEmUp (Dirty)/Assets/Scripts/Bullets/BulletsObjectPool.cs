12df78e16f81ed18c6e40b97533ba1735730f2c3
Petrov Dmitry <petrovdmtry@gmail.com>
2025-02-16 13:27:38 +0300

    init

--- 4. GRASP/ShootEmUp (Dirty)/Assets/Scripts/Bullets/BulletsObjectPool.cs
+++
@@ -1,8 +0,0 @@
-using ShootEmUp.Modules.Base;
-
-namespace ShootEmUp
-{
-    public sealed class BulletsObjectPool : ObjectPool<Bullet>
-    {
-    }
-}
--- 4. GRASP/ShootEmUp (Dirty)/Assets/Scripts/Enemy/EnemyObjectPool.cs
+++
@@ -1,5 +0,0 @@
-using ShootEmUp;
-using ShootEmUp.Modules.Base;
-
-public class EnemyObjectPool : ObjectPool<Enemy>
-{ }
